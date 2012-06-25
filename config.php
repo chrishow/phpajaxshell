f45aa750da5c77f3ebbf7de263d1806e8577b3c1
Chris How <chris@primesolid.com>
2012-06-25 17:38:14 +0200

    Removed example content

--- config.php
+++ config.php
@@ -8,6 +8,6 @@
 
 
 $config = array(
-    'username' => 'chris',
-    'hashed_password' => '$2a$07$Lb5rsOeiYI95tPbj3RCxs.o4yl5/WG3eDe4A4fwniXNcBfLPniMB6:Lb5rsOeiYI95tPbj3RCxsD'
+    'username' => '',
+    'hashed_password' => ''
 );
